d1365b78360949e4fc4d6856c9adc5d2d60c6507
EstherOA <estherowusuansah19@gmail.com>
2025-06-14 23:54:52 +0000

    add scroll to section animation

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,8 +4,34 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
 } from "../ui/navigation-menu";
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollToPlugin);
 
 const Navbar = () => {
+  useEffect(() => {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const target = link.getAttribute("href");
+        if (!target || target === "/") return;
+
+        gsap.to(window, {
+          duration: 1,
+          scrollTo: {
+            y: target,
+            offsetY: 70,
+          },
+          ease: "power2.inOut",
+        });
+      });
+    });
+  }, []);
+
   return (
     <NavigationMenu viewport={false} orientation="horizontal">
       <NavigationMenuList>
